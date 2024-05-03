26d8a8ecc6c5aae2d0d705da99a1110f7a9a335a
astechedu <astech.utube@gmail.com>
2024-05-04 02:48:38 +0530

    Js, node, express Shopping2

--- js-vite-shop/header.js
+++
@@ -1,5 +0,0 @@
-export function header(headerId){
-	headerId.innerHTML = `
-	  <div>This is header</div>
-	`
-}
--- js-vite-shop/site.js
+++
@@ -1,5 +0,0 @@
-export const Rotue = () => {
-    // Get the current URL
-    var currentUrl = window.location.pathname;
-    console.log("Current URL:", currentUrl);   
-}
